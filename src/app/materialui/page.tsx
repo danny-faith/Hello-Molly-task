8e70e0e2cce5af57d1e928f16ff01323caa38b7b
daniel blythe <daniel.blythe@oohmedia.com.au>
2023-11-01 11:13:08 +1100

    MUI demo page added

---
+++ src/app/materialui/page.tsx
@@ -0,0 +1,54 @@
+"use client";
+import { Button, Drawer, Slider } from "@mui/material";
+import { list } from "postcss";
+import React from "react";
+// CODE COPIED FROM MUI docs
+
+type Anchor = "top" | "left" | "bottom" | "right";
+
+export default function MaterialUI() {
+  const [state, setState] = React.useState({
+    top: false,
+    left: false,
+    bottom: false,
+    right: false,
+  });
+
+  const toggleDrawer =
+    (anchor: Anchor, open: boolean) =>
+    (event: React.KeyboardEvent | React.MouseEvent) => {
+      if (
+        event.type === "keydown" &&
+        ((event as React.KeyboardEvent).key === "Tab" ||
+          (event as React.KeyboardEvent).key === "Shift")
+      ) {
+        return;
+      }
+
+      setState({ ...state, [anchor]: open });
+    };
+  return (
+    <>
+      {(["left", "right", "top", "bottom"] as const).map((anchor) => (
+        <React.Fragment key={anchor}>
+          <Button onClick={toggleDrawer(anchor, true)}>{anchor}</Button>
+          <Drawer
+            anchor={anchor}
+            open={state[anchor]}
+            onClose={toggleDrawer(anchor, false)}
+          >
+            {<p>hello</p>}
+          </Drawer>
+        </React.Fragment>
+      ))}
+      <div>
+        <Slider defaultValue={30} />
+        <Slider
+          defaultValue={30}
+          className="text-teal-600"
+          slotProps={{ thumb: { className: "rounded-sm" } }}
+        />
+      </div>
+    </>
+  );
+}
