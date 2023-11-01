c20bc809818bac251eca70e1e01a131320d4a29e
daniel blythe <daniel.blythe@oohmedia.com.au>
2023-11-01 22:00:24 +1100

    added inital data structure and hardcoded fetched serverside

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,27 +1,20 @@
-import SearchRounded from "@mui/icons-material/SearchRounded";
-import { Paper } from "@mui/material";
-import Box from "@mui/material/Box";
-import Button from "@mui/material/Button";
-import Input from "@mui/material/Input";
+import { Hero } from "./components/Hero";
+import hierarchy from "./hierarchy.json";
 
-export default function Home() {
+async function getHiearchyServerSide() {
+  "use server";
+  return hierarchy;
+}
+
+export default async function Home() {
+  const hierarchy = await getHiearchyServerSide();
+
+  console.log("hierarchy", hierarchy);
+
   return (
-    <Box
-      className="flex flex-col items-center justify-between py-14 md:py-52 lg:py-60"
-      sx={{
-        backgroundImage:
-          "url(https://upload.wikimedia.org/wikipedia/commons/b/b7/Download_Thousands_Of_Free_Stunning_Stock_Photos._%2824031439750%29.jpg)",
-        backgroundSize: "cover",
-        backgroundPosition: "center",
-      }}
-    >
-      <Paper sx={{ padding: 2 }}>
-        <Input
-          size="medium"
-          startAdornment={<SearchRounded />}
-          endAdornment={<Button sx={{ px: 4 }}>Search</Button>}
-        />
-      </Paper>
-    </Box>
+    <div>
+      <Hero />
+      <pre>{JSON.stringify(hierarchy, null, 2)}</pre>
+    </div>
   );
 }
