3c320791780d396d3b4df04bf24d723cab54cb70
daniel blythe <daniel.blythe@oohmedia.com.au>
2023-11-05 07:23:57 +1100

    org chart moved to homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,30 @@
 import { Hero } from "../components/Hero";
 import hierarchy from "./hierarchy";
-import { Box } from "@mui/material";
-import { Typography } from "@mui/joy";
+import { Box, Grid } from "@mui/material";
+import CompanyHierarchy from "@/components/CompanyHierarchy";
+import { TreeViewDemo } from "@/components/TreeView";
+import { HierarchyProvider } from "./context/HierarchyContext";
 
 async function getHiearchyServerSide() {
-  "use server";
   return hierarchy;
 }
 
 export default async function Home() {
   const hierarchy = await getHiearchyServerSide();
 
-  console.log("hierarchy", hierarchy);
-
   return (
     <Box>
-      <Hero data={hierarchy} />
-      <Box py={5}>
-        <Typography>Home</Typography>
-      </Box>
-      {/* <pre>{JSON.stringify(hierarchy, null, 2)}</pre> */}
+      <HierarchyProvider>
+        <Hero data={hierarchy} />
+        <TreeViewDemo data={hierarchy} />
+        <Grid container justifyContent="center">
+          <Grid xs={12} md={8} lg={12}>
+            <Box py={5}>
+              <CompanyHierarchy data={hierarchy} setSelected={""} />
+            </Box>
+          </Grid>
+        </Grid>
+      </HierarchyProvider>
     </Box>
   );
 }
